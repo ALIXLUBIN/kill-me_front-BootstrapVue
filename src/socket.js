c0aeb5e9b5e78429db706337420b15879b83ade5
Lubin ALIX-VOISINE <114068754+ALIXLUBIN@users.noreply.github.com>
2024-03-29 11:28:16 +0100

    update ws ip

--- src/socket.js
+++ src/socket.js
@@ -8,8 +8,8 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-	process.env.NODE_ENV === "production" ? undefined : process.env.VUE_APP_WS_IP;
+const URL = process.env.VUE_APP_WS_IP;
+	// process.env.NODE_ENV === "production" ? undefined : process.env.VUE_APP_WS_IP;
 
 export const socket = io(URL, {
     withCredentials: true,
